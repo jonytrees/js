990167dd93e8ce27816533e9de31fdd3e8c08cd7
JonyTrees <jonytrees@gmail.com>
2019-10-03 23:26:07 +0200

    03-10 дома: 12 урок пройден и немного попрактиковался в scriptHome

--- scriptHome.js
+++ scriptHome.js
@@ -1,17 +1,40 @@
 var menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
-
-function multiplyNumeric(obj){
-    for(var key in obj){
-        if(typeof obj[key] == 'string'){
-            continue;
-        }
-        obj[key] *= 2;
-        alert(obj[key]);
-    }
+    name: '',
+    age: 13,
+    method: function(){
+        alert('menu');
+    },
 }
 
-multiplyNumeric(menu);
+Object.defineProperty(menu, 'name', { // Object.defineProperty служит для определения атрибутов
+    value: 'tom',
+    writable: false, // доступность свойства для записи, возможность переопределить
+    enumerable: false, // доступность перечисления для цикла for(in), доступность к свойству
+    configurable: true, // доступность свойства для настройки, если false до ниже нельзя будет преопределить атрибуты ниже
+});
+
+var obj = {
+    subname : 'smith',
+    __proto__ : menu,
+
+}
+
+obj.func2 = function(){
+    return this.subname + ' ' + this.name;
+};
+
+obj.name = 'cole';
+
+// obj.method = 'obj';
+
+
+// for(var key in obj){
+//     if(typeof obj[key] == 'function'){
+//         alert(obj[key]);
+//     }
+// }
+
+alert(obj.func2());
+
+
+
